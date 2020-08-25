aecffae7ca9826259c4f47d313c791c53ff8e282
step_ne <nemail.necom@gmail.com>
2020-08-25 17:44:28 +0300

    HW4

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -5,6 +5,7 @@ import HW2 from "../../../p2-homeworks/h2/HW2";
 import HW3 from "../../../p2-homeworks/h3/HW3";
 import Message from "../../../p2-homeworks/h1/Message";
 import Menu from "../../../p2-homeworks/h1/Menu";
+import HW4 from "../../../p2-homeworks/h4/HW4";
 
 
 function App() {
@@ -19,8 +20,9 @@ function App() {
             {/*<HW1/>*/}
             {/*<HW2/>*/}
             {/*<HW3/>*/}
-           <Message/>
-            <Menu title="Menu"/>
+           {/*<Message/>*/}
+           {/* <Menu title="Menu"/>*/}
+            <HW4/>
         </div>
     );
 }
