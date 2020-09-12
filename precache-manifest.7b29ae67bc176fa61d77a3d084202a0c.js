bf79a3fa20c2c185e2257f7052a5f08cdaad03a5
Stepan-Ne <nemail.necom@gmail.com>
2020-09-12 14:45:32 +0300

    Updates

---
+++ precache-manifest.7b29ae67bc176fa61d77a3d084202a0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da45f3ddcf69e1716a4cfd51709793eb",
+    "url": "/HomeTasks/index.html"
+  },
+  {
+    "revision": "226d9ab575ba36e42857",
+    "url": "/HomeTasks/static/css/main.b9f0da3c.chunk.css"
+  },
+  {
+    "revision": "5026acf675050b8f6ba7",
+    "url": "/HomeTasks/static/js/2.05a52b3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeTasks/static/js/2.05a52b3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "226d9ab575ba36e42857",
+    "url": "/HomeTasks/static/js/main.90d24d37.chunk.js"
+  },
+  {
+    "revision": "fc9da1f14a51a7ac31be",
+    "url": "/HomeTasks/static/js/runtime-main.6241284f.js"
+  }
+]);
