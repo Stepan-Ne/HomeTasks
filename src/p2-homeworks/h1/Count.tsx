cf02c1b574e9b6bcf74bd76fa945ccc6a77016d1
step_ne <nemail.necom@gmail.com>
2020-09-13 20:54:34 +0300

    Fixed counter.

--- src/p2-homeworks/h1/Count.tsx
+++ src/p2-homeworks/h1/Count.tsx
@@ -1,43 +1,67 @@
-import React, {MouseEvent, useState} from "react";
+import React, {FormEvent} from "react";
 
 type PropsType = {
     countValue: number
-    incrementValue: ()=> void
+    incrementValue: () => void
+    resetValue: () => void
+    flag: boolean
 }
 const count = {
     borderRadius: "5px",
     backgroundColor: "#f2f2f2",
     padding: "20px",
     maxWidth: "200px",
-}
+};
 const value = {
     fontSize: "2rem",
     border: "1px solid black",
     padding: "5px",
-}
+};
 const buttons = {
     border: "1px solid black",
     marginTop: "2px",
     padding: "10px"
-}
-
+};
+const button = {
+    border: "1px solid black",
+    padding: "10px",
+    borderRadius: "2px",
+    display: "inline-block",
+    marginLeft: "20px"
+};
+const flagStyle = {
+    color: "orange",
+    fontSize: "2.5rem",
+    border: "1px solid black",
+    padding: "5px",
+};
 
 const Count: React.FC<PropsType> = (props) => {
 
     const increment = () => {
         props.incrementValue();
-    }
+    };
+    const reset = () => {
+        props.resetValue();
+    };
+    const onChangeValueHandler = (e: FormEvent<HTMLDivElement>) => {
+        console.log(e.currentTarget);
+    };
 
     return <div style={count}>
 
-        <div style={value}>
+        <div style={props.flag ? flagStyle : value} onChange={onChangeValueHandler}>
             {props.countValue}
         </div>
         <div style={buttons}>
-            <button onClick={increment}>inc</button>
-            <button>reset</button>
+            <div style={button}>
+                <button disabled={props.flag} onClick={increment}>inc</button>
+            </div>
+            <div style={button}>
+                <button disabled={!props.flag} onClick={reset}>reset</button>
+            </div>
         </div>
     </div>
-}
+};
 
 export default Count;
--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -4,19 +4,27 @@ import Count from "./Count";
 
 
 function HW1() {
-
+    const [flag, setFlag] = useState(false);
     const [value, setValue] = useState(0);
+    const maxValue = 4;
     const incrementValue = () => {
-        if (value < 5) {
-            setValue(value + 1);
-        } else {
-            setValue(0);
-        }
-
-    }
+       if (value <= maxValue) {
+           setValue(value + 1);
+       }
+       if (value === maxValue) {
+           setFlag(true);
+           return;
+       }
+    };
+    const resetValue = () => {
+        setValue(0);
+        setFlag(false);
+    };
     return (
         <div>
             <Count countValue={value}
+                   flag={flag}
+                   resetValue={resetValue}
                    incrementValue={incrementValue}
             />
         </div>
